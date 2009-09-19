34393111579861bfdbe42855830cdd5fd5db9ec6
Uri Laserson <laserson@mit.edu>
2009-09-19 17:48:59 -0400

    Bug fix

--- bin/vdj_full_pipeline_LSF.py
+++ bin/vdj_full_pipeline_LSF.py
@@ -9,8 +9,8 @@ import vdj
 parser = optparse.OptionParser()
 parser.add_option('-q','--queue')
 parser.add_option('-o','--LSFoutput')
+parser.add_option('-p','--packetsize',type='int')
 parser.add_option('-b','--barcodes',dest='barcodes_fasta')
-parser.add_option('-c','--cutoff',default=4.5,type='float')
 parser.add_option('-i','--IGHC',dest='ighc_fasta')
 parser.add_option('-m','--min',type='int')
 parser.add_option('-M','--max',type='int')
@@ -24,7 +24,7 @@ else:
 
 cmd1 = 'fasta2vdjxml.py'
 cmd2 = 'size_select.py --min %d --max %d' % (options.min,options.max)
-cmd3 = 'vdjxml2parts --packetsize 500 --basename'
+cmd3 = 'vdjxml2parts --packetsize 500 --basename %s' % args[0]
 cmd = ' | '.join([cmd1,cmd2,cmd3])
 p = subprocess.Popen(cmd,shell=True,stdin=inhandle,stdout=subprocess.PIPE)
 parts = [f.strip() for f in p.stdout.readlines()]
