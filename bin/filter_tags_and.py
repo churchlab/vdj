4da1604eb77e258b85a59d1b5b1fb1ccb24e7ad6
Uri Laserson <laserson@mit.edu>
2009-09-21 22:40:20 -0400

    Additional helper scripts

---
+++ bin/filter_tags_and.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import sys
+import optparse
+
+import vdj
+
+parser = optparse.OptionParser()
+parser.add_option('-t','--tag',action='append',dest='tags')
+(options, args) = parser.parse_args()
+
+if len(args) == 2:
+    inhandle = open(args[0],'r')
+    outhandle = open(args[1],'w')
+elif len(args) == 1:
+    inhandle = open(args[0],'r')
+    outhandle = sys.stdout
+elif len(args) == 0:
+    inhandle = sys.stdin
+    outhandle = sys.stdout
+
+query_tags = set(options.tags)
+for chain in vdj.parse_VDJXML(inhandle):
+    if query_tags <= chain.all_tags:
+        print >>outhandle, chain
