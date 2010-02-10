d4368976865a20fd582b6180932ce80bd9471bbc
Brendan Hickey <brendan@bhickey.net>
2010-02-10 14:13:40 -0500

    First pass at the Bayes Classifier.

--- hashcore.c
+++ hashcore.c
@@ -17,6 +17,202 @@ unsigned short getNucleotide(char ch){
     }
 }
 
+OddsTable::OddsTable(int n){
+    _size = n;
+    _contains = (double*) malloc(sizeof(double) * n);
+    _doesnot  = (double*) malloc(sizeof(double) * n);
+    _featurelessOdds = 0;
+    for( int c = 1; c <= n ; c++ ){
+        double cGivenF   = -1 * log(c + 1);
+        double cGivenNF  = -1 * log(c * (n - c + 1));
+
+        _contains[n-1] = cGivenF;
+        _doesnot[n-1]  = cGivenNF;
+        _featurelessOdds += cGivenNF;
+        //printf("p(c|f) = %f\tp(c|~f)= %f (%d, %d)\n", cGivenF, cGivenNF, c, n);       
+        //ncGivenF  = -1 * log((c + 1) * (n - c));
+        //ncGivenNF = -1 * log(n - c + 1);
+    }
+    printf("null score: %f\n", _featurelessOdds);
+}
+
+OddsTable::~OddsTable(){
+    free(_contains);
+    free(_doesnot);
+}
+
+double OddsTable::getFeaturelessOdds(){ return _featurelessOdds; }
+
+double OddsTable::oddsGivenFeature(int c){
+
+    assert( c > 0 && c <= _size );
+    //printf("%d: %f\n", (c - 1), _contains[c-1]);
+    return _contains[c-1];
+}
+
+double OddsTable::oddsWithoutFeature(int c){
+    assert( c > 0 && c <= _size );
+    return _doesnot[c - 1];
+}
+
+/*
+ * FeatureSet
+ */
+/*
+char* FeatureSet::getName() {
+    return string(_name);
+}
+
+char* FeatureSet::getSequence() {
+    
+    return string(_sequence);
+}
+*/
+/*
+ * ReferenceSet
+ */
+
+ReferenceSet::ReferenceSet(list<ObservationSet*> observations ){
+
+    _refs = new map<unsigned long, int>();
+    _withOdds = new map<unsigned long, double>();
+    _withoutOdds = new map<unsigned long, double>();
+
+    list<ObservationSet*>::iterator obs_itr;
+    set<unsigned long>::iterator feat_itr;
+    
+    _size = observations.size();
+
+    //TODO First get it working, then get it right. I can do this in linear time.
+    //     I'm taking O(n lg n)
+    for( obs_itr = observations.begin() ; obs_itr != observations.end() ; obs_itr++ ){
+        set<unsigned long>* features = (*obs_itr)->getFeatureSet();
+        
+        for( feat_itr = features->begin() ; feat_itr != features->end() ; feat_itr++ ){
+           insertBump(_refs, (*feat_itr)); 
+        }
+    }
+
+    _odds = new OddsTable(_size);
+    
+    map<unsigned long, int>::iterator ref_itr;
+   
+    for( ref_itr = _refs->begin(); ref_itr != _refs->end() ; ref_itr++ ){
+        double with    = _odds->oddsGivenFeature((*ref_itr).second); 
+        double without = _odds->oddsWithoutFeature((*ref_itr).second); 
+        
+        _withOdds->insert(pair<unsigned long, double>((*ref_itr).first, with));
+        _withoutOdds->insert(pair<unsigned long, double>((*ref_itr).first, without));
+    
+    }
+    // NOTE pseudocounts are accomodated for in the odds calculation
+    //addPseudocounts();
+
+}
+
+double ReferenceSet::getNull(){ return _odds->getFeaturelessOdds(); }
+
+/*
+bool ReferenceSet::addPseudocounts(){
+    
+    // check to see if pseudocounts have already been added
+    if( _pseudocounts ){
+        return false;
+    }
+
+    map<unsigned long, int>::iterator o_itr;
+
+    for( o_itr = _refs->begin(); o_itr != _refs->end() ; o_itr++ ){
+       (*o_itr).second += 1; 
+    }
+    _pseudocounts = true;
+    return true;
+}
+
+*/
+
+/*
+ * ObservationSet: Raw observation
+ */
+
+ObservationSet::ObservationSet(char *name, char *sequence){
+    int nm_len = strlen(name);
+    int seq_len = strlen(sequence);
+
+    _name = (char*) malloc(nm_len + 1);
+    _sequence = (char*) malloc(seq_len + 1);
+    strcpy(_name, name);
+    strcpy(_sequence, sequence);
+    _obsset = extractFeatures(_sequence, new map<unsigned long, int>());
+   // printf("Observation Set size: %d\n", _obsset->size());
+}
+ObservationSet::ObservationSet(string name, string sequence){
+    ObservationSet( name.c_str(), sequence.c_str() );
+    return;
+}
+
+//TODO call the supertype deconstructor
+ObservationSet::~ObservationSet(){
+    delete _obsset;
+    free(_sequence);
+    free(_name);
+}
+
+set<unsigned long>* ObservationSet::getFeatureSet() {
+    map<unsigned long, int>::iterator f_itr;
+    set<unsigned long> *features = new set<unsigned long>();
+
+    for( f_itr = _obsset->begin(); f_itr != _obsset->end(); f_itr++ ){
+        features->insert((*f_itr).first);    
+    }
+
+    return features;
+}
+
+/*
+ * LikelihoodSet: Observations in log-likelihood space
+ */
+LikelihoodSet::LikelihoodSet(ObservationSet* obs, ReferenceSet* ref){
+   
+    map<unsigned long, double>::iterator ref_itr;
+    set<unsigned long>::iterator obs_itr; 
+    set<unsigned long> *obs_features = obs->getFeatureSet();
+
+    _lset = new map<unsigned long, double>();
+
+    //printf("obs length: %d\n", obs->_obsset->size());
+    //TODO First get it working, then get it right.
+    //TODO Make this robust -- handle keys not found
+    for(obs_itr = obs_features->begin() ; obs_itr != obs_features->end(); obs_itr++){
+       double val = (*ref->_withOdds->find(*obs_itr)).second - (*ref->_withoutOdds->find(*obs_itr)).second;
+
+       printf("%f\n", val);
+       _lset->insert(pair<unsigned long, double>(*obs_itr, val));
+    }
+
+    _nullOdds = ref->getNull();
+    delete obs_features;
+}
+
+double LikelihoodSet::likelihood(ObservationSet* os){
+
+    map<unsigned long, int>::iterator os_itr;
+    map<unsigned long, double>::iterator lset_itr;
+    double like = _nullOdds;
+    for( os_itr = os->_obsset->begin() ; os_itr != os->_obsset->end(); os_itr++ ){
+        lset_itr = _lset->find(os_itr->first);
+
+        if( lset_itr != _lset->end() ){
+            like += lset_itr->second;
+        }
+    }
+
+    return like;
+    
+
+}
+
+
 // This code was not generated by hand. Modifying it is ill-advised.
 inline void runCombs( map<unsigned long, int>* hm, unsigned long xx, unsigned long mask, int ii ){
 
@@ -35,7 +231,9 @@ inline void insertBump(map<unsigned long, int>* mp, unsigned long key){
 
     // Are we in the right place?
     if( (*itr).first == key ){
-        (*itr).second += 1; // bump the count if we are
+        int val = (*itr).second + 1;
+        mp->erase(key);
+        mp->insert(pair<unsigned long, int>(key, val));
     } else {
         // otherwise add it to the set
         mp->insert(itr, pair<unsigned long, int>(key, 1));
@@ -48,7 +246,7 @@ void pseudocount(map<unsigned long, int>* mp){
     map<unsigned long, int>::iterator mp_itr;
 
     for( mp_itr = mp->begin(); mp_itr != mp->end(); mp_itr++ ){
-        (*mp_itr) += 1;
+        (*mp_itr).second += 1;
     }
 
     return;
@@ -78,7 +276,13 @@ map<unsigned long, int>* extractFeatures(char* seq, map<unsigned long, int> *res
 
         runCombs(res, acc, nmask, ii);    
     }
+   
+    map<unsigned long, int>::iterator res_itr;
+    for( res_itr = res->begin() ; res_itr != res->end() ; res_itr++){
+        printf("%ld -> %d\n", (*res_itr).first, (*res_itr).second);
+    }
     
+    printf("Map size: %d\n", (int) res->size());
     return res;
 }
 
@@ -122,9 +326,25 @@ void initalignmentcore() {
 
 int main(int argc, char** argv){
     
-    char* seq = "CGATATGCACGCTCTCGACGCGCGACGCACGACGACACACGAGAGNGNGNGACGACGACGGACGACGA";
+    char* seq = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
+    list<ObservationSet*> obsList;
+    for(int ii = 0; ii < 1 ; ++ii ){
+        obsList.push_back(new ObservationSet("Test", seq));    
+    }
+
+    ObservationSet *observations = new ObservationSet("Obs", seq);
+    ReferenceSet *ref = new ReferenceSet(obsList);
+    LikelihoodSet *ls = new LikelihoodSet(observations, ref);
+
+
+
+    printf("Null: %f\n", ref->getNull());
+    printf("Likelihood: %f\n", ls->likelihood(observations));
+    /*
     list<char*> sequences;
     sequences.push_back(seq);
+
     map<unsigned long, int>* f = extractFeatures(seq);
 
     map<unsigned long, int>::iterator itr;
@@ -134,6 +354,9 @@ int main(int argc, char** argv){
     }
 
     delete f;
+    */
+
+    delete observations;
     return 0;
 
 }
--- hashcore.h
+++ hashcore.h
@@ -5,11 +5,86 @@
 #include <string>
 #include <iterator>
 
+#include <assert.h>
+#include <math.h>
 #include <stdlib.h>
 
 // #include <Python.h>
 
+// TODO Use templates so that I can abstract out the feature sets and inherit from the supertype
+
 enum nucleotide {A,T,C,G};
+
+class OddsTable {
+    public:
+        OddsTable(int);
+        ~OddsTable();
+
+        double getFeaturelessOdds();
+        double oddsGivenFeature(int);
+        double oddsWithoutFeature(int);
+    private:
+        int     _size;
+        double _featurelessOdds;
+        double *_contains;
+        double *_doesnot;
+};
+
+class FeatureSet {
+    public:
+        std::string getName();
+        std::set<unsigned long> *getFeatureSet();
+    protected:
+        char *_name;
+};
+
+class LikelihoodSet;
+class ObservationSet;
+class ReferenceSet;
+
+class ReferenceSet: public FeatureSet {
+    public:
+        ReferenceSet(std::list<ObservationSet*>);
+        bool addObservation(ObservationSet*);
+        int size();
+        double getNull();
+        LikelihoodSet *makeLikelihood(ObservationSet*);
+        friend class LikelihoodSet;
+    protected:
+        std::map<unsigned long, int> *_refs;
+        std::map<unsigned long, double> *_withOdds;
+        std::map<unsigned long, double> *_withoutOdds;
+ 
+    private:
+        int _size;
+        OddsTable *_odds;
+        double _nullOdds;
+        bool addPseudocounts();
+        bool _pseudocounts;
+};
+
+class ObservationSet: public FeatureSet {
+    public:
+        ObservationSet(char *name, char *sequence);
+        ObservationSet(std::string name, std::string sequence);
+        ~ObservationSet();
+        std::set<unsigned long> *getFeatureSet();
+    protected:
+        std::map<unsigned long, int>* _obsset;
+    private:
+        char *_sequence;
+        friend class LikelihoodSet;
+};
+
+class LikelihoodSet: public FeatureSet {
+    public:
+        LikelihoodSet(ObservationSet* obs, ReferenceSet* corpus);
+        ~LikelihoodSet();
+       double likelihood(ObservationSet*);
+    private:
+        std::map<unsigned long, double>* _lset; 
+        double _nullOdds;
+};
 
 
 
