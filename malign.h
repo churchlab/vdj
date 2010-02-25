65098b60bad859aa253767c602564bda40251dca
Brendan Hickey <brendan@bhickey.net>
2010-02-25 15:33:56 -0500

    Fixed a bug in priority_queue usage. Comparison was being done on pointers, not objects.

--- malign.h
+++ malign.h
@@ -29,7 +29,7 @@ typedef struct{
 
 class MAlignerEntry {
             public:
-                MAlignerEntry(std::string, std::string, dp_matrix *dp);
+                MAlignerEntry(std::string, std::string, dp_matrix *dp, int);
                 ~MAlignerEntry();
                 bool initialize(std::string, int);
                 bool isAligned();
@@ -39,12 +39,11 @@ class MAlignerEntry {
                 int upperBound();
                 int lowerBound();
                 int getScore();
+                int getId(){ return _id; }
                 int gap, match, mismatch;
                 std::string getName();
-                const bool operator< (MAlignerEntry&);
-                const bool operator> (MAlignerEntry&);
             private:
-
+                int _id;
                 int scoreDP(int, int, int, int*, int*);
                 std::string _name;
                 char *refSequence;
@@ -76,7 +75,24 @@ class MAlignerEntry {
                 bool aligned;
                 bool initialized;
 };
- 
+
+bool operator> (MAlignerEntry& lhs, MAlignerEntry& rhs){
+    return lhs.getScore() < rhs.getScore(); 
+}
+bool operator< (MAlignerEntry& lhs, MAlignerEntry& rhs){
+    return lhs.getScore() < rhs.getScore();
+}
+
+struct CompareMEntry : public std::binary_function<MAlignerEntry*, MAlignerEntry*, bool>
+{
+    bool operator()(MAlignerEntry* x, MAlignerEntry* y) const
+    {
+        return *x < *y;
+    }
+};
+
+typedef std::priority_queue<MAlignerEntry*, std::vector<MAlignerEntry*>, CompareMEntry> MAE_queue;
+
 class MAlignerCore {
     public:
       
@@ -84,11 +100,12 @@ class MAlignerCore {
         ~MAlignerCore();
         void addEntry(std::string name, std::string sequence);
         std::string bestAlign(std::string input);
-        std::priority_queue<MAlignerEntry*> align(std::string input);
+        MAE_queue align(std::string input);
         //std::priority_queue<MAlignerEntry*> alignWith(char* input, int nnames, char** names);
-        std::priority_queue<MAlignerEntry*> roundRobin(std::queue<MAlignerEntry*>);
+        MAE_queue roundRobin(std::queue<MAlignerEntry*>);
 
     private:
+        int _id;
         std::map<std::string,MAlignerEntry*> entries;
         dp_matrix *global_matrix;
 };
