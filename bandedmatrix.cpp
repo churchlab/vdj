279a270032b630f0a00bf0b6c8672058bf787e8c
Brendan Hickey <brendan@bhickey.net>
2010-03-03 14:55:35 -0500

    Basic BandedMatrix API

--- bandedmatrix.cpp
+++ bandedmatrix.cpp
@@ -4,15 +4,123 @@
 
 using namespace std;
 
+/*
+ * BandedMatrixIterator
+ */
+
+template <class T>
+BandedMatrixIterator<T>::BandedMatrixIterator(){
+    _size = 0;
+    _index = 0;
+    _x = 0;
+    _y = 0;
+}
+
+template <class T>
+BandedMatrixIterator<T>::BandedMatrixIterator(const BandedMatrixIterator<T> &itr){
+    _cols = itr._cols;
+    _currCol = itr._currCol;
+    _size = itr._size;
+    _index = itr._index;
+    _x = itr._x;
+    _y = itr._y;
+    _dat = itr._dat;
+}
+
+template <class T>
+BandedMatrixIterator<T>::BandedMatrixIterator(vector<MatrixCol<T> > &cols, int size){
+    _currCol = cols[0];
+    _dat = _currCol._dat;
+    _cols = cols;
+    _x = 0;
+    _y = 0;
+    _size = size;
+
+    
+}
+
+template <class T>
+BandedMatrixIterator<T>::~BandedMatrixIterator(){
+
+}
+
+template <class T>
+bool BandedMatrixIterator<T>::operator==(BandedMatrixIterator<T> &itr){
+
+    return( _dat == itr._dat && _index == itr._index );
+
+}
+
+template <class T>
+bool BandedMatrixIterator<T>::operator!=(BandedMatrixIterator<T> &itr){
+    return !( _dat == itr._dat || _index != itr._index );
+}
+
+template <class T>
+bool BandedMatrixIterator<T>::hasNext(){
+    return _index < _size - 1;
+}
+
+template <class T>
+bool BandedMatrixIterator<T>::hasPrev(){
+    return _index > 0;
+}
+
+template <class T>
+BandedMatrixIterator<T>& BandedMatrixIterator<T>::operator++() {
+    _y++;
+    _index++;
+    if( _y - _currCol._offset - 1 > _currCol._length ){
+        _x++;
+        _currCol = _cols[_x];
+        _y = _currCol._offset;
+    }
+    return *this;
+}
+
+template <class T>
+BandedMatrixIterator<T>& BandedMatrixIterator<T>::operator--() {
+    _y--;
+    _index--;
+    if( _y - _currCol._offset < 0 ){
+        _x--;
+        _currCol = _cols[_x];
+        _y = _currCol._offset;
+    }
+    return *this;
+}
+
+
+template <class T>
+T& BandedMatrixIterator<T>::operator*() const {
+    printf("%p + %d = %p\n", _dat, _index, _dat + _index);
+    return _dat[_index];
+}
+
+template <class T>
+pair<int, int> BandedMatrixIterator<T>::coordinates(){
+    return pair<int, int>(_x,_y);
+}
+
+/*
+ * MatrixCol: Convenient addressing of matrix rows
+ */
 template <class S>
-MatrixRow<S>::MatrixRow(int offset, int length, S* row){
+MatrixCol<S>::MatrixCol(){
+    _offset = 0;
+    _length = 0;
+    _dat = NULL;
+}
+
+template <class S>
+MatrixCol<S>::MatrixCol(int offset, int length, S* row){
     _offset = offset;
     _length = length;
     _dat = row;
 }
 
 template <class S>
-S& MatrixRow<S>::operator[](const int ii){
+S& MatrixCol<S>::operator[](const int ii){
     if( ii - _offset >= 0 && ii - _offset < _length){
         return _dat[ii - _offset]; 
     } else {
@@ -27,36 +135,45 @@ BandedMatrix<T>::BandedMatrix(int x, int y){
     _left  = (_numCols < _numRows ? _numRows - _numCols : 1);
     _right = (_numRows < _numCols ? _numCols - _numRows : 1);
     
-    _cols = vector<T*>(_numCols);
+    _cols = vector<MatrixCol<T> >(_numCols);
     _startPos = vector<int>(_numCols);
     resize(false, false);
     _dat = (T*) malloc(sizeof(T) * _size);
     makeCols();
+    _begin = BandedMatrixIterator<T>(_cols, _size);
+    _end   = BandedMatrixIterator<T>(_cols, _size);
 }
 
 template <class T>
-MatrixRow<T> BandedMatrix<T>::operator[](const int ii){
-    int offset = _startPos[ii];
-    int length = (int) (ii == _numRows - 1 ? (_dat + _size) - _cols[ii] : _cols[ii + 1] - _cols[ii]);
-    
-    if( ii >= 0 && ii < _numCols ){
-        return MatrixRow<T>(offset, length, _cols[ii]);
-    } else {
-        raise(SIGSEGV);
+MatrixCol<T> BandedMatrix<T>::operator[](const int ii){
+    return _cols[ii];
+}
+
+template<class T>
+bool BandedMatrix<T>::inbounds(int x, int y){
+    if( x < 0 || x >= _numCols || y < 0 || y >= _numRows ){
+        return false;
     }
+
+    MatrixCol<T> column = _cols[x];
+    
+    if( y - column._offset < 0 || y - column._offset >= column._length ){
+        return false;
+    }
+
+    return true;
+
 }
 
 template <class T>
 void BandedMatrix<T>::makeCols(){
-
     int colLength = 1 + _left;
     T* index = _dat; 
     int effectiveRight = 0;
     int effectiveLeft  = _left;
     int colStart = 0;
    for(int ii = 0; ii < _numCols; ++ii ){
-        _cols[ii] = index;
-        _startPos[ii] = colStart;
+        _cols[ii] = MatrixCol<T>(colStart, colLength, index);
         
         if( ii + _left + 1 >= _numRows && effectiveLeft >= 0 ){
             effectiveLeft--;
@@ -72,7 +189,18 @@ void BandedMatrix<T>::makeCols(){
             + effectiveRight 
             + effectiveLeft;
         index += colLength;
-   }
+
+    } 
+}
+
+template <class T>
+const BandedMatrixIterator<T> BandedMatrix<T>::begin(){
+    return _begin;
+}
+
+template <class T>
+const BandedMatrixIterator<T> BandedMatrix<T>::end(){
+    return _end;
 }
 
 template <class T>
@@ -100,58 +228,4 @@ int BandedMatrix<T>::resize(bool gLeft, bool gRight){
         (lowerTriangle * (lowerTriangle + 1)) / 2;
     assert(_size > 0);   
     return _size;
-}
-
-template <class T>
-T BandedMatrix<T>::set(int x, int y, T v){
-    printf("Setting (%d, %d) at ", x, y);
-    T* ptr = getPtr(x,y);
-    printf("%p\n", ptr);
-    *ptr = v;
-    return *ptr;
-}
-
-template <class T>
-T BandedMatrix<T>::get(int x, int y){
-    printf("Getting (%d, %d) at ", x, y);
-    T* ptr = (getPtr(x,y));
-    printf("%p\n", ptr);
-    return *ptr;
-}
-
-template <class T>
-T* BandedMatrix<T>::getPtr(int x, int y){
-    T* col_ptr = _cols[x];
-    int offset = _startPos[x];
-    int index  = x - offset;
-
-    if( index < 0 ){
-        return NULL;    
-    }
-    
-    if( index > _size ){
-        return NULL;
-    }
-
-    if( x > _numCols || x < 0 ){
-        return NULL;
-    }
-
-    if( y > _numRows || y < 0 ){
-        return NULL;
-    }
-
-    return (col_ptr + index);
-
-}
-
-int main(int argc, void** argv){
-
-    BandedMatrix<int> m = BandedMatrix<int>(5,5);
-    printf("Set %d\n", m.set(4,4,1));
-    printf("Got %d\n", m.get(4,4));
-    printf("%d\n", m[4][4]);
-    m[4][4] = -1;
-    printf("%d\n", m[4][4]);
-    return 0; 
 }
