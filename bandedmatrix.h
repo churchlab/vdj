284e914cc4c47da0928c153ad6dca5fca90381db
Brendan Hickey <brendan@bhickey.net>
2010-03-03 14:55:35 -0500

    Basic BandedMatrix API

--- bandedmatrix.h
+++ bandedmatrix.h
@@ -4,16 +4,51 @@
 #include <utility>
 #include <vector>
 
-template <class S>
-class MatrixRow
+template <class T>
+class MatrixCol;
+
+template <class T>
+class BandedMatrixIterator
 {
     public:
-        MatrixRow(int, int, S*);
-        S& operator[](const int);
+        BandedMatrixIterator();
+        BandedMatrixIterator(const BandedMatrixIterator<T> &itr);
+        BandedMatrixIterator(std::vector<MatrixCol<T> > &itr, int size);
+        ~BandedMatrixIterator();
+        //FIXME these operators are broken. fix em.
+        bool operator== (BandedMatrixIterator<T> &itr); 
+        bool operator!= (BandedMatrixIterator<T> &itr); 
+        
+        bool hasNext();
+        bool hasPrev();
+
+        //FIXME these operators only work as prefix ops.
+        BandedMatrixIterator<T>& operator++();
+        BandedMatrixIterator<T>& operator--();
+        T& operator*() const;
+        std::pair<int, int> coordinates();
     private:
+        T* _dat;
+        std::vector<MatrixCol <T> > _cols;
+        MatrixCol<T> _currCol;
+        int _size;
+        int _x;
+        int _y;
+        int _index;
+};
+
+template <class S>
+class MatrixCol
+{
+    public:
+        MatrixCol();
+        MatrixCol(int, int, S*);
+        S& operator[](const int);
+    protected:
+        friend class BandedMatrixIterator<S>;
+        S* _dat;
         int _offset;
         int _length;
-        S*  _dat;
 };
 
 template <class T>
@@ -22,14 +57,15 @@ class BandedMatrix
 
     public:
         BandedMatrix(int, int);
-        MatrixRow<T> operator[](const int);
-        T get(int, int);
-        T set(int, int, T);
+        MatrixCol<T> operator[](const int);
+        const BandedMatrixIterator<T> begin();
+        const BandedMatrixIterator<T> end();
+        bool inbounds(int, int);
         int resize(bool, bool);
     private:
-        T* _dat; //used shared swap space later;
+        T* _dat; //TODO use shared swap space later;
         T  _val;
-        std::vector<T*> _cols;
+        std::vector<MatrixCol<T> > _cols;
         std::vector<int> _startPos; 
         void makeCols();
         T* getPtr(int, int);
@@ -40,5 +76,8 @@ class BandedMatrix
         int _size;
         int _left;
         int _right;
+
+        BandedMatrixIterator<T> _begin;
+        BandedMatrixIterator<T> _end;
 };
 
