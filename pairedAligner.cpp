405277d985accae5830a4cce63db3b6cc8e0f173
Brendan Hickey <brendan@bhickey.net>
2010-03-12 15:11:39 -0500

    Paired aligner uses dynamic programming.

--- pairedAligner.cpp
+++ pairedAligner.cpp
@@ -1,10 +1,54 @@
 #include <assert.h>
 
+#include <algorithm>
 #include <queue>
 #include <vector>
 #include <string>
 
 using namespace std;
+
+double phredTable[] = {-0.6931471805599453,-0.10536051565782628,-1.005033585350145e-2,-1.0005003335835344e-3,-1.0000500033334732e-4,-1.0000050000287824e-5,-1.000000500029089e-6,-1.0000000494736474e-7,-1.0000000100247594e-8,-9.999999722180686e-10,-1.000000082790371e-10};
+
+class LLCell {
+    public:
+        LLCell(){
+            _base = '-';
+            _quality = 0;
+            _likelihood = 0.0;
+        }
+        LLCell(double l, char b, int q){
+            _base = b;
+            _quality = q;
+            _likelihood = l;
+            _match = false;
+        }
+        LLCell(char chrA, int qualA, char chrB, int qualB){
+            _match = false;
+            if( chrA == chrB ){
+                _quality = qualA + qualB;
+                _base = chrA;
+                _match = true;
+            } else if( qualA >= qualB ){
+                _quality = qualA - qualB;
+                _base = chrA;
+            } else {
+                _quality = qualB - qualA;
+                _base = chrB;
+            }
+            _likelihood = phredTable[_quality/10];
+        }
+        void adjustLikelihood(double adj){ _quality += adj; }
+        char getBase(){ return _base; }
+        int  getQuality(){ return _quality; }
+        double getLikelihood(){ return _likelihood; }
+        bool isMatch(){ return _match; }
+    private:
+        char _base;
+        int _quality;
+        double _likelihood;
+        bool _match;
+
+};
 
 class qread {
     public:
@@ -13,7 +57,12 @@ class qread {
 };
 
 
-int binomialTable[] = { 0, 1, 2, 3, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 10, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 15, 16, 16, 16, 17, 17, 18, 18, 18, 19, 19, 19, 20, 20, 20, 21, 21, 22, 22, 22, 23, 23, 23, 24, 24, 24, 25, 25, 25, 26, 26, 26, 27, 27, 27, 28, 28, 28, 29, 29, 29, 30, 30, 30, 31, 31, 31 };
+    
+    int binomialTable[] = { 0, 1, 2, 3, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 10, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 15, 16, 16, 16, 17, 17, 18, 18, 18, 19, 19, 19, 20, 20, 20, 21, 21, 22, 22, 22, 23, 23, 23, 24, 24, 24, 25, 25, 25, 26, 26, 26, 27, 27, 27, 28, 28, 28, 29, 29, 29, 30, 30, 30, 31, 31, 31 };
+
+bool significantMatch(int matches, int overlap){
+    return matches > binomialTable[overlap];
+}
 
 bool goodBase(char n){
     switch(n){
@@ -52,7 +101,7 @@ char complement(char n){
         case 't': return 'a';
         case 'T': return 'A';
         case 'g': return 'c';
-        case 'G': return 'C';
+case 'G': return 'C';
         default:  return n;
     }
 }
@@ -65,6 +114,10 @@ string reverse_complement(string s){
     return res;
 }
 
+double phred2log(int q){
+    return phredTable[q/10];
+
+}
 
 qread align(qread readA, qread readB){
 
@@ -77,96 +130,103 @@ qread align(qread readA, qread readB){
     vector<int> qualB = readB._qual;
     reverse(qualB.begin(), qualB.end());
 
+    int lenA = seqA.size();
+    int lenB = seqB.size();
 
-    //printf("SeqA: %s (%d)\nSeqB: %s (%d)\n", seqA.c_str(), seqA.size(), seqB.c_str(), seqB.size());
-    int minLen = min(seqA.size(), seqB.size());
-    priority_queue<pair<double, int> > res;
+    LLCell dpm[lenA + 1][lenB + 1];
 
-     for(int ii = 5 ; ii <= minLen; ii++){
-        int hits = 0;
-        //printf("%d\t%d\n", seqA.size() - ii, ii);
-        //printf("%s\n%s\n\n", seqA.substr(seqA.size() - ii, ii).c_str(), seqB.substr(0,ii).c_str());
-        for(int jj = 0; jj < ii; jj++){
-            char baseA = seqA[seqA.size() - 1 - jj];
-            char baseB = seqB[ii - jj - 1];
-            //printf("%c %c (%d %d)\n", baseA, baseB, seqA.size() - 1 - jj, ii - jj);
-            if( goodBase(baseA) && goodBase(baseB) ){
-                hits += (normalizeBase(baseA) == normalizeBase(baseB) ? 1 : 0);
-            }
-        }
+    dpm[0][0] = LLCell(
+            phred2log(qualA[0]),
+            seqA[0],
+            qualA[0]);
 
-        if( hits > binomialTable[ii] ){
-            double score = (double) hits / (double) ii;
-            //printf("Pushing <%f,%d>\n", score, ii);
-            res.push(pair<double, int>(score, ii));
+    int ii, jj;
+    for(ii = 1; ii < lenA; ii++){
+       dpm[ii][0] =
+           LLCell(
+               dpm[ii - 1][0].getLikelihood() + phred2log(qualA[ii]),
+                seqA[ii],
+                qualA[ii]);
+    }
+
+    dpm[ii][0] =
+        LLCell(
+            dpm[ii - 1][0].getLikelihood() + phred2log(0),
+            '-',
+            0);
+
+    for(jj = 1; jj < lenB; jj++){
+        for(ii = 0; ii < lenA - 1; ii++){
+            double likelihood;
+            char chrA = seqA[ii];
+            char chrB = seqB[jj];
+            char consensusBase;
+            int qual;
+            dpm[ii][jj] = LLCell(
+                    chrA,
+                    qualA[ii],
+                    chrB,
+                    qualB[jj]);
+            dpm[ii][jj].adjustLikelihood(dpm[ii - 1][jj - 1].getLikelihood());
         }
     }
 
-        qread result;
-        int overlap;
+    for(ii = 1; ii < lenB; ii++){
+        dpm[lenA][ii] =
+            LLCell(
+                dpm[lenA][ii - 1].getLikelihood() + phred2log(qualB[ii]),
+                seqB[ii],
+                qualB[ii]);
+    }
 
-        int res_length = 0;
-        // If we have no evidence that indicates that our paired reads overlap:
-        //  i.e. no one crossed the binomial threshhold.
-        if( res.empty() ){
-            overlap = -1;
 
-            res_length = seqA.size() + seqB.size() + 1;
-            result._qual.reserve(res_length);
-            result._qual = qualA;
-            result._seq = seqA;
-            // insert a Q00 gap between the sequences
-            result._seq += '-';
-            result._qual.push_back(0);
+    ii = lenB;
+    jj = lenA;
 
-            result._seq += readB._seq; // tack on the other read
-            result._qual.insert(result._qual.end(), qualB.begin(), qualB.end());
+    string consensus;
+ 
+    int matches = 0;
+    int overlap = 0;
+
+    printf("Log-Likelihood: %f\n", dpm[ii][jj].getLikelihood());
+    while( ii > 0 && jj > 0 ){
+
+        double upLikelihood   = dpm[ii][jj - 1].getLikelihood();
+        double leftLikelihood = dpm[ii - 1][jj].getLikelihood();
+        double diagLikelihood = dpm[ii - 1][jj - 1].getLikelihood();
+
+        consensus += dpm[ii][jj].getBase();
+        if( diagLikelihood >= upLikelihood &&
+            diagLikelihood >= leftLikelihood ){ 
+            
+            if( dpm[ii][jj].isMatch() ){
+                matches++;
+            }
+            overlap++;
+
+            ii--;
+            jj--;
+        } else if( upLikelihood >= leftLikelihood ){
+            jj--;
         } else {
-            overlap = res.top().second;
-            res_length = seqA.size() + seqB.size() - overlap;
-            result._qual.reserve(res_length);
-            result._seq = seqA.substr(0, seqA.size() - overlap);
-            printf("%s\n", result._seq.c_str());
-            string bPart = seqB.substr(overlap, seqB.size() - overlap);
-
-            printf("%s\n", bPart.c_str());
-
-            result._qual.insert(result._qual.end(), qualA.begin(), qualA.end() - overlap);
-
-            int al = seqA.size();
-            int bl = seqB.size();
-
-            for(int ii = 0; ii < overlap; ii++ ){
-                int aind = al - overlap + ii;
-                if( seqA[aind] == seqB[ii] ){
-                    result._seq += seqB[ii];
-                    result._qual.push_back(qualA[aind] + qualB[ii]);
-                } else {
-                    if( qualA[aind] >= qualB[ii] ){
-                        result._seq += seqA[aind];
-                    } else {
-                        result._seq += seqB[ii];
-                    }
-                        result._qual.push_back(0);
-                }
-            }
-
-            if( overlap < bl ){
-                result._seq += bPart;
-                result._qual.insert(result._qual.end(), qualB.begin() + overlap, qualB.end());
-            }
+            ii--;
         }
+    }
 
-        printf("Assembly: %s (%d)\n", result._seq.c_str(), (int) result._seq.size());
-        printf("Qual length: %d\n", (int) result._qual.size());
-        vector<int>::iterator q_itr;
+    while(ii > 0){
+        consensus += dpm[ii][jj].getBase();
+        ii--;
+    }
 
-        for( q_itr = result._qual.begin(); q_itr != result._qual.end(); q_itr++ ){
-            printf("%d ", *q_itr);
-        }
-        printf("\n");
-        //assert(result._qual.size() == result._seq.size());
-        return result;
+    while(jj > 0){
+        consensus += dpm[ii][jj].getBase();
+        jj--;
+    }
+
+    printf("%s\n", (significantMatch(overlap, matches) ? "SIGNIFICANT" : "NOT SIGNIFICANT"));
+    reverse(consensus.begin(), consensus.end());
+    return readA;
+
 }
 
 int main(int argc, char **argv){
@@ -174,9 +234,9 @@ int main(int argc, char **argv){
 
     
     polyA._seq = string("AAACCCTTTT");
-    polyA._qual = vector<int>(polyA._seq.size(),10);
+    polyA._qual = vector<int>(polyA._seq.size(),50);
     polyT._seq = string("GGGAAAAGG");
-    polyT._qual = vector<int>(polyT._seq.size(),10);
+    polyT._qual = vector<int>(polyT._seq.size(),50);
     qread r = align(polyA, polyT);
     printf("%s\n", r._seq.c_str());
     return 0;
