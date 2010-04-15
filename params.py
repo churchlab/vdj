4835e07e93e0c4251c6d8546985fb2f705481f6f
Uri Laserson <laserson@mit.edu>
2010-04-14 22:46:59 -0400

    Rebuilding params.py from the bottom up

--- params.py
+++ params.py
@@ -7,12 +7,23 @@ to point to certain resources.
 # directory that contains imgt.dat, imgt.fasta, imgtrefseq.fasta from IMGT
 # NOTE: after unzipping IMGT.zip, it bungles imgt.dat and imgt.fasta.
 #       These need to be deleted and the .Z version SEPARATELY uncompressed.
-refdatadir = '/Users/laserson/research/church/vdj-ome/ref-data/IMGT'
-imgtdat = 'imgt.dat'
-imgtfasta = 'imgt.fasta'
-imgtvseq = 'vdj_ref.fasta'
-imgtrefseqfasta = 'imgtrefseq.fasta'
-imgtspecfasta  = 'imgtspec.fasta'
-imgtspecvdjxml = 'imgtspec.vdjxml'
-imgtrefseqFR3endcoords = 'imgtrefseqFR3endcoords.dat'
-imgtrefseqJTRPstartcoords = 'imgtrefseqJTRPstartcoords.dat'
+#
+# NOTE: this should be fixed to not require manual pointing to IMGT
+
+# full IMGT flatfile database dir
+imgtdir = '/Users/laserson/research/church/vdj-ome/ref-data/IMGT'
+
+# packaged data dir
+datadir = 'data'
+
+
+
+# refdatadir = '/Users/laserson/research/church/vdj-ome/ref-data/IMGT'
+# imgtdat = 'imgt.dat'
+# imgtfasta = 'imgt.fasta'
+# imgtvseq = 'vdj_ref.fasta'
+# imgtrefseqfasta = 'imgtrefseq.fasta'
+# imgtspecfasta  = 'imgtspec.fasta'
+# imgtspecvdjxml = 'imgtspec.vdjxml'
+# imgtrefseqFR3endcoords = 'imgtrefseqFR3endcoords.dat'
+# imgtrefseqJTRPstartcoords = 'imgtrefseqJTRPstartcoords.dat'
