a4071974c3b974698e0f13c13a0fe2ea337e221f
Uri Laserson <laserson@mit.edu>
2010-08-03 15:41:28 -0400

    script to convert old VDJXML to new VDJXML

---
+++ bin/update_vdjxml.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import sys
+
+import vdj
+
+if len(sys.argv) == 3:
+    inhandle = open(sys.argv[1],'r')
+    outhandle = open(sys.argv[2],'w')
+elif len(sys.argv) == 2:
+    inhandle = open(sys.argv[1],'r')
+    outhandle = sys.stdout
+elif len(sys.argv) == 1:
+    inhandle = sys.stdin
+    outhandle = sys.stdout
+
+for chain in vdj.legacy.parse_VDJXML_old(inhandle):
+    print >>outhandle, chain
