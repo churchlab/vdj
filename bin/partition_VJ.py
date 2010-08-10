dd2cf59b5dede5e29adc6fe055df0195786fae3c
Uri Laserson <laserson@mit.edu>
2010-08-10 15:36:46 -0400

    Script to partition vdjxml into VJ combos

---
+++ bin/partition_VJ.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+import sys
+import optparse
+import subprocess
+import os
+import tempfile
+
+import seqtools
+
+import vdj
+
+parser = optparse.OptionParser()
+parser.add_option('-b','--basename')
+(options, args) = parser.parse_args()
+
+if len(args) == 1:
+    inhandle = open(args[0],'r')
+elif len(args) == 0:
+    inhandle = sys.stdin
+
+# NOTE: this script ignores the allele numbers
+
+def vj_id_no_allele(chain):
+    return seqtools.cleanup_id(chain.v.split('*')[0]) + '_' + seqtools.cleanup_id(chain.j.split('*')[0])
+
+for chain in parse_VDJXML(inhandle):
+    curr_vj_id = vj_id_no_allele(chain)
+    try:
+        print >>outhandles[curr_vj_id], chain
+    except KeyError:
+        outhandles[curr_vj_id] = open("%s.%s.vdjxml" % (options.basename,curr_vj_id),'w')
+        print >>outhandles[curr_vj_id], "<root>"
+        print >>outhandles[curr_vj_id], chain
+
+for outhandle in outhandles.itervalues():
+    print >>outhandle, "</root>"
