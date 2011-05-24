cbcf052c6b310da479e744006e77aadc6a79b568
Uri Laserson <uri.laserson@gmail.com>
2011-05-24 15:43:11 -0400

    Readded functionality for VDJXML parsing.
    Also added fn to convert from VDJXML to IMGT formatted chains.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 import types
+import xml.etree.cElementTree as ElementTree    # for VDJXML parsing
 
 from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
 from Bio.SeqRecord import SeqRecord
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 
@@ -218,3 +221,175 @@ def filter_parse_imgt(inputfile,predicate):
         chain = ImmuneChain(record)
         if predicate(chain):
             yield chain
+
+
+# ==========================================================================
+
+# =============================
+# = TO PARSE OLD VDJXML FILES =
+# =============================
+
+class ImmuneChainXML(object):
+    """Data structure to represent an immune chain."""
+    
+    def __init__(self,**kw):
+        """Initialize ImmuneChain
+        
+        seq is 5'->3'
+        """
+        def kw_init(attrib):
+            if kw.has_key(attrib):
+                self.__setattr__(attrib,kw[attrib])
+        
+        kw_init('seq')
+        kw_init('descr')
+        kw_init('locus')
+        kw_init('v')
+        kw_init('d')
+        kw_init('j')
+        kw_init('c')
+        kw_init('junction')
+        
+        if kw.has_key('tags'):
+            tags = kw['tags']
+            if isinstance(tags,types.StringTypes): tags = [tags]
+            self.tags = set(tags)
+        else:
+            self.tags = set([])
+    
+    def get_cdr3(self):
+        return len(self.junction)
+    def set_cdr3(self,value):
+        pass
+    cdr3 = property(fget=get_cdr3,fset=set_cdr3)
+    
+    def get_vj(self):
+        return '|'.join([self.v,self.j])
+    def set_vj(self):
+        pass
+    vj = property(fget=get_vj,fset=set_vj)
+    
+    def get_vdj(self):
+        return '|'.join([self.v,self.d,self.j])
+    def set_vdj(self):
+        pass
+    vdj = property(fget=get_vdj,fset=set_vdj)
+    
+    def add_tags(self,tagset):
+        if isinstance(tagset,types.StringTypes): tagset = [tagset]
+        self.tags.update(tagset)
+    
+    def add_tag(self,tag):
+        self.add_tags(tag)
+    
+    def remove_tags(self,tagset):
+        if isinstance(tagset,types.StringTypes): tagset = [tagset]
+        for tag in tagset: self.tags.remove(tag)
+    
+    def remove_tag(self,tag):
+        self.remove_tags(tag)
+    
+    def has_tag(self,tag):
+        if tag in self.tags:
+            return True
+        else:
+            return False
+    
+    def __len__(self):
+        return len(self.seq)
+    
+    def __str__(self):
+        return self.__repr__()
+    
+    def __repr__(self):
+        return self.get_XML()
+    
+    def get_XML(self):
+        format_xml = lambda attrib,value: "\t<%(attrib)s>%(value)s</%(attrib)s>\n" % {'attrib':attrib,'value':value}
+        xmlstring = '<ImmuneChain>\n'
+        for (attrib,value) in self.__dict__.iteritems():
+            if attrib == 'tags':
+                for tag in self.tags:
+                    xmlstring += format_xml('tag',tag)
+            else:
+                xmlstring += format_xml(attrib,value)
+        xmlstring += '</ImmuneChain>\n'
+        return xmlstring
+
+
+class ParserVDJXML(object):
+    """Parser for VDJXML"""
+    def __init__(self):
+        self.chain = None
+    
+    def start_handler(self,elem):
+        if elem.tag == 'ImmuneChain':
+            self.chain = ImmuneChainXML()
+    
+    def end_handler(self,elem):
+        if elem.tag == 'tag':
+            self.chain.add_tags(elem.text)
+        elif elem.tag == 'v_end_idx' or elem.tag == 'j_start_idx':
+            self.chain.__setattr__(elem.tag,int(elem.text))
+        else:
+            self.chain.__setattr__(elem.tag,elem.text)
+    
+    def parse(self,inputfile):
+        for event, elem in ElementTree.iterparse(inputfile,events=('start','end')):
+            if event == 'start':
+                if elem.tag == 'root':  # to ensure non-incorp of <root> obj in chain
+                    pass
+                else:
+                    self.start_handler(elem)
+            elif event == 'end':
+                if elem.tag == 'ImmuneChain':
+                    yield self.chain
+                elif elem.tag == 'root':    # to ensure clean exit at end of file
+                    pass
+                else:
+                    self.end_handler(elem)
+
+
+class PredicateParserVDJXML(ParserVDJXML):
+    """VDJXML Parser that takes a predicate function"""
+    def __init__(self,predicate):
+        ParserVDJXML.__init__(self)
+        self.predicate = predicate
+    
+    def parse(self,inputfile):
+        for event, elem in ElementTree.iterparse(inputfile,events=('start','end')):
+            if event == 'start':
+                self.start_handler(elem)
+            elif event == 'end':
+                if elem.tag == 'ImmuneChain':
+                    if self.predicate(self.chain) == True:
+                        yield self.chain
+                else:
+                    self.end_handler(elem)
+
+
+def parse_VDJXML(inputfile):
+    vdjxmlparser = ParserVDJXML()
+    return vdjxmlparser.parse(inputfile)
+
+
+def filter_parse_VDJXML(inputfile,predicate):
+    vdjxmlparser = PredicateParserVDJXML(predicate)
+    return vdjxmlparser.parse(inputfile)
+
+def xml2imgt(chainXML):
+    seq = Seq(chainXML.seq,generic_dna)
+    chain = ImmuneChain(seq=seq,id=chainXML.descr,name=chainXML.descr,description=chainXML.descr)
+    chain.annotations['barcode'] = chainXML.barcode
+    chain.add_tags(list(chainXML.tags))
+    chain.annotations['clone'] = chainXML.clone
+    if "coding" not in chain.tags: raise ValueError, "I want coding chains only."
+    vfeature = SeqFeature(location=FeatureLocation(0,chainXML.v_end_idx),type='V-REGION',strand=1,qualifiers={"allele":[chainXML.v]})
+    jfeature = SeqFeature(location=FeatureLocation(chainXML.j_start_idx,len(seq)),type='J-REGION',strand=1,qualifiers={"allele":[chainXML.j]})
+    cdr3feature = SeqFeature(location=FeatureLocation(chainXML.v_end_idx+3,chainXML.j_start_idx-3),type='CDR3-IMGT',strand=1)
+    chain.features.append(vfeature)
+    chain.features.append(jfeature)
+    chain.features.append(cdr3feature)
+    chain._update_feature_dict()
+    chain._process_source_feature()
+    return chain
