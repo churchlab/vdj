a21a0181f0cde723776be103d413cfa8bb80552b
Uri Laserson <uri.laserson@gmail.com>
2012-01-13 13:04:37 -0500

    Fixed my idiocy

--- bin/imgt2junctions.py
+++ bin/imgt2junctions.py
@@ -9,6 +9,8 @@ outfile = sys.argv[2]
 
 with open(outfile,'w') as op:
     for chain in vdj.parse_imgt(infile):
-        try: junction = chain.junction_nt
-        except AttributeError: pass
+        try:
+            op.write( ">%s\n%s\n" % (chain.id,chain.junction_nt) )
+        except AttributeError:
+            pass
 
