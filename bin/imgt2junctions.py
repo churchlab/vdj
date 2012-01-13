06ab026b629381f5bb7e53d5a9df93334b5b7a10
Uri Laserson <uri.laserson@gmail.com>
2012-01-13 16:22:07 -0500

    Updated cmd line parsing

--- bin/imgt2junctions.py
+++ bin/imgt2junctions.py
@@ -1,16 +1,30 @@
 #! /usr/bin/env python
 
 import sys
+import argparse
 
 import vdj
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
-with open(outfile,'w') as op:
-    for chain in vdj.parse_imgt(infile):
-        try:
-            op.write( ">%s\n%s\n" % (chain.id,chain.junction_nt) )
-        except AttributeError:
-            pass
+argparser = argparse.ArgumentParser(description=None)
+argparser.add_argument('positional',type=int,nargs='+')
+args = argparser.parse_args()
+
+if len(args.positional) == 2:
+    inhandle = open(args[0],'r')
+    outhandle = open(args[1],'w')
+elif len(args.positional) == 1:
+    inhandle = open(args[0],'r')
+    outhandle = sys.stdout
+elif len(args.positional) == 0:
+    inhandle = sys.stdin
+    outhandle = sys.stdout
+
+for chain in vdj.parse_imgt(inhandle):
+    try:
+        op.write( ">%s\n%s\n" % (chain.id,chain.junction_nt) )
+    except AttributeError:
+        pass
 
