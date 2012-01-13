0f102e98362799189ea52701cb46cb3321986659
Uri Laserson <uri.laserson@gmail.com>
2012-01-13 16:24:01 -0500

    bugfix

--- bin/imgt2junctions.py
+++ bin/imgt2junctions.py
@@ -13,10 +13,10 @@ argparser.add_argument('positional',type=int,nargs='+')
 args = argparser.parse_args()
 
 if len(args.positional) == 2:
-    inhandle = open(args[0],'r')
-    outhandle = open(args[1],'w')
+    inhandle = open(args.positional[0],'r')
+    outhandle = open(args.positional[1],'w')
 elif len(args.positional) == 1:
-    inhandle = open(args[0],'r')
+    inhandle = open(args.positional[0],'r')
     outhandle = sys.stdout
 elif len(args.positional) == 0:
     inhandle = sys.stdin
