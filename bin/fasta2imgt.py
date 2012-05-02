bf8d5d13360ef7dbfcfe9966cc4b59202d857a35
Uri Laserson <uri.laserson@gmail.com>
2012-05-01 21:41:09 -0700

    Updated fasta2imgt from optparse to argparse

--- bin/fasta2imgt.py
+++ bin/fasta2imgt.py
@@ -1,29 +1,18 @@
 #! /usr/bin/env python
 
 import sys
-import optparse
+import argparse
 
 from Bio import SeqIO
 from Bio.Alphabet import generic_dna
 
 import vdj
 
-parser = optparse.OptionParser()
-parser.add_option('-D','--debug',action='store_true')
-(options, args) = parser.parse_args()
+argparser = argparse.ArgumentParser(description=None)
+argparser.add_argument('input',nargs='?',type=argparse.FileType('r'),default=sys.stdin)
+argparser.add_argument('output',nargs='?',type=argparse.FileType('w'),default=sys.stdout)
+args = argparser.parse_args()
 
-if len(args) == 2:
-    inhandle = open(args[0],'r')
-    outhandle = open(args[1],'w')
-elif len(args) == 1:
-    inhandle = open(args[0],'r')
-    outhandle = sys.stdout
-elif len(args) == 0:
-    inhandle = sys.stdin
-    outhandle = sys.stdout
-else:
-    raise Exception, "Wrong number of arguments."
-
-for record in SeqIO.parse(inhandle,'fasta',generic_dna):
+for record in SeqIO.parse(args.input,'fasta',generic_dna):
     chain = vdj.ImmuneChain(record.upper())
-    print >>outhandle, chain
+    print >>args.output, chain
